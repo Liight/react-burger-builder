6eb81a6856d8c51de3d04cdd15d32f82df3210c7
Liight <Liight@users.noreply.github.com>
2018-12-27 13:01:46 +1300

    Handling overall Form validity
    Updated button.css styles and conditional styles logic button.js.  Added overall form validation logic and passed props to form elements.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,9 +85,12 @@ class ContactData extends Component {
                         ]
                     },
                     value: '',
+                    validation: {},
+                    valid: true,
                     touched: false
                 },
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -115,11 +118,15 @@ class ContactData extends Component {
             .catch(error => {
                 this.setState({loading: false});
             });
-        console.log('orderHandler', this.props.ingredients)
     }
 
     checkValidity (value, rules) {
         let isValid = true;
+
+        if(!rules){
+            return true;
+        }
+
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
@@ -147,8 +154,13 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement)
-        this.setState({orderForm: updatedOrderForm});
+
+        let formIsValid = true;
+        for(let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render () {
@@ -173,7 +185,7 @@ class ContactData extends Component {
                             changed={(event) => this.inputChangedHandler(event, formElement.id)}
                             touched={formElement.config.touched}/>
                     ))}
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                    <Button btnType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
                 </form>
         );
         if (this.state.loading){
