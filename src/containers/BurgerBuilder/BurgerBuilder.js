7280e21aa5e234f148af6b700b2603386771758d
Liight <Liight@users.noreply.github.com>
2019-01-04 10:52:49 +1300

    Adjusting Checkout and ContactData to use redux
    Removed queryParams from purchaseContinuedHanlder in BurgerBuilder.js, replaced with simple history.push(). Connected Checkout.js and ContactData.js to Redux global state.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -58,20 +58,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinuedHandler = () => {
-    
-
-        // Iterate over this.state.ingredients and push URL friendly versions to queryParams
-        const queryParams = [];
-        for(let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&') // Join the array with &
-        //Send object to history
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        }); //redirect via history object
+        this.props.history.push('/checkout');
     }
 
     render() {
