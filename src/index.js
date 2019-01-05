814d4c518e9db00e187110f7115ed02d0b5e7d28
Liight <Liight@users.noreply.github.com>
2019-01-05 14:44:58 +1300

    Installing and implementing thunk to compose async actions

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
+
+// Adding middleware
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Second argument in createStore enables google browser REDUX devtools extension 
 const store = createStore(
     burgerBuilderReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
     );
 
 // Enabled Routing
