7f7926fb7435420300b65a3de1d32e5b229087eb
Liight <Liight@users.noreply.github.com>
2019-01-07 10:52:08 +1300

    Working on order Actions
    Action types, action creators for orders actions.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,8 +93,7 @@ class ContactData extends Component {
                     touched: false
                 },
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = ( event ) => {
@@ -194,7 +193,7 @@ class ContactData extends Component {
                     <Button btnType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
                 </form>
         );
-        if (this.state.loading){
+        if (this.props.loading){
             form = <Spinner />
         }
         return (
@@ -209,13 +208,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalprice
+        price: state.totalprice,
+        loading: state.loading
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
