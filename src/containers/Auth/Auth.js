288e0d71f6bc41330d709297a24d17cf634042e1
Liight <Liight@users.noreply.github.com>
2019-01-08 08:20:21 +1300

    Adding Auth Actions
    Added action types and actions. updated import file (index.js) with new actions. Updated Auth.js with actions, required imports and handlers.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
-
-import * as classes from './Auth.css';
+import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 
 class Auth extends Component {
@@ -87,6 +88,12 @@ class Auth extends Component {
         this.setState({controls: updatedControls})
     };
 
+    // Handles the form submission
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    };
+
     render () {
 
         // Iterate over each key in state.orderform and push a javascript object to formElementsArray
@@ -119,7 +126,7 @@ class Auth extends Component {
         // Add a Submit Button element below the form variable but within the HTML form element
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -128,4 +135,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
