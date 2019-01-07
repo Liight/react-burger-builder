0d43e8fd8915ec3d00d9f25dc150c076aaedf4ef
Liight <Liight@users.noreply.github.com>
2019-01-08 10:19:22 +1300

    Updating the UI depending on Auth State
    Using the Redux store token, pass props to the Layout component, and conditionally render the Authenticate/Logout routes if user is logged in or logged out.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../_Aux/_Aux';
 import classes from './Layout.css';
@@ -24,8 +25,11 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <Toolbar 
+                isAuth={this.props.isAuthenticated}
+                drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer 
+                isAuth={this.props.isAuthenticated}
                     closed={this.sideDrawerClosedHandler}
                     open={this.state.showSideDrawer} />
                 <main className={classes.Content}>
@@ -36,4 +40,11 @@ class Layout extends Component {
     }
 };
 
-export default Layout;
+// Checks if the user is authenticated or not via the presense of a token from the google firebase api upon login
+const mapStateToprops = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToprops)(Layout);
