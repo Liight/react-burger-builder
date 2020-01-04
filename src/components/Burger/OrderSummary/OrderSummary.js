eebb5a41b62bfed3f189a827fa2b2e23dfac78e2
Liight <Liight@users.noreply.github.com>
2020-01-05 11:23:31 +1300

    Style Update: - OrderSummary - Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,24 +3,37 @@ import React, { Component } from 'react'
 import Aux from '../../../hoc/_Aux/_Aux';
 import Button from '../../UI/Button/Button';
 
+// Material UI
+import { withStyles } from "@material-ui/styles";
+import 'typeface-roboto';
+import Typography from '@material-ui/core/Typography';
+
+const styles = () => ({
+    typography: {
+        fontSize: "1.4em",
+        textAlign: "center"
+    }
+});
+
 class OrderSummary extends Component {
     render () {
+        const { classes } = this.props;
         const ingredientSummary = Object.keys(this.props.ingredients)
         .map((igKey) => {
             return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                <li key={igKey} style={{listStyleType: "none"}}>
+                    <Typography className={classes.typography}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</Typography>
                 </li> );
         } );
         return (
             <Aux>
-                <h3>Your Order</h3>
-                <p>A delicious burger with the following ingredients:</p>
-                <ul>
+                <Typography className={classes.typography}><h3>Your Order</h3></Typography>
+                <Typography className={classes.typography}><p>A delicious burger with the following ingredients:</p></Typography>
+                <ul style={{marginLeft: 0, paddingLeft: 0}}>
                     {ingredientSummary}
                 </ul>
-                <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
-                <p>Continue to Checkout?</p>
+                <Typography className={classes.typography}><p><strong>Total price: {this.props.price.toFixed(2)}</strong></p></Typography>
+                <Typography className={classes.typography}><p>Continue to Checkout?</p></Typography>
                 <div style={{width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-evenly"}}>
                     <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
                     <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
@@ -30,4 +43,4 @@ class OrderSummary extends Component {
     }
 }
 
-export default OrderSummary;
+export default withStyles(styles)(OrderSummary);
